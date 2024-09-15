4111c40137dfc7dcaae257f4a05b0e18c985a894
arinal <dicky.arinal@gmail.com>
2024-09-15 13:45:05 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::{
+    collections::HashSet,
+    io::{self, Write},
+};
+
+use anyhow::{Error, Result};
 
 fn main() -> io::Result<()> {
     let stdin = io::stdin();
@@ -8,17 +12,38 @@ fn main() -> io::Result<()> {
         print!("$ ");
         io::stdout().flush()?;
         let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        stdin.read_line(&mut input)?;
         let mut it = input.trim().split_whitespace();
 
         match it.next() {
-            Some("exit") => break Ok(()),
-            Some("echo") => {
-                let rest = it.collect::<Vec<&str>>().join(" ").trim().to_string();
-                println!("{}", rest);
-            }
-            Some(unknown) => println!("{}: command not found", unknown),
-            None => (),
+            Some("echo") => handle_echo(it),
+            Some("exit") => handle_exit(it),
+            Some("type") => handle_type(it),
+            Some(unknown) => Err(Error::msg(format!("{}: command not found", unknown))),
+            None => Err(Error::msg("unexpected end of command input")),
         }
+        .unwrap_or_else(|e| eprintln!("{}", e));
+    }
+}
+fn handle_echo(it: std::str::SplitWhitespace) -> Result<()> {
+    let rest = it.collect::<Vec<&str>>().join(" ").trim().to_string();
+    Ok(println!("{}", rest))
+}
+
+fn handle_exit(mut it: std::str::SplitWhitespace) -> Result<()> {
+    let code = it.next().unwrap_or("0").parse::<i32>()?;
+    std::process::exit(code);
+}
+
+fn handle_type(mut it: std::str::SplitWhitespace) -> Result<()> {
+    let arg = it
+        .next()
+        .ok_or_else(|| Error::msg("type: missing argument"))?;
+    match arg {
+        arg if HashSet::from(["echo", "exit", "type"]).contains(arg) => {
+            println!("{} is a shell builtin", arg);
+            Ok(())
+        }
+        _ => Err(Error::msg(format!("{}: not found", arg))),
     }
 }
