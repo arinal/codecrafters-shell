4f9784cf39df8ae0455a0bd8286a4bbebb8ab81e
arinal <dicky.arinal@gmail.com>
2024-09-15 15:06:14 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,66 @@
 use std::{
     collections::HashSet,
     io::{self, Write},
+    str::SplitWhitespace,
 };
 
 use anyhow::{Error, Result};
 
 fn main() -> io::Result<()> {
-    let stdin = io::stdin();
+    let binding = std::env::var("PATH").unwrap_or_else(|_| "/bin:/usr/bin".into());
+    let paths = binding.split(':').collect::<HashSet<_>>();
 
     loop {
-        print!("$ ");
-        io::stdout().flush()?;
-        let mut input = String::new();
-        stdin.read_line(&mut input)?;
+        let input = prompt()?;
         let mut it = input.trim().split_whitespace();
-
         match it.next() {
             Some("echo") => handle_echo(it),
             Some("exit") => handle_exit(it),
-            Some("type") => handle_type(it),
+            Some("type") => handle_type(it, &paths),
             Some(unknown) => Err(Error::msg(format!("{}: command not found", unknown))),
             None => Err(Error::msg("unexpected end of command input")),
         }
         .unwrap_or_else(|e| eprintln!("{}", e));
     }
 }
-fn handle_echo(it: std::str::SplitWhitespace) -> Result<()> {
-    let rest = it.collect::<Vec<&str>>().join(" ").trim().to_string();
+
+fn prompt() -> io::Result<String> {
+    let stdin = io::stdin();
+    print!("$ ");
+    io::stdout().flush()?;
+    let mut input = String::new();
+    stdin.read_line(&mut input)?;
+    Ok(input)
+}
+
+fn handle_echo(tokens: SplitWhitespace) -> Result<()> {
+    let rest = tokens.collect::<Vec<&str>>().join(" ").trim().to_string();
     Ok(println!("{}", rest))
 }
 
-fn handle_exit(mut it: std::str::SplitWhitespace) -> Result<()> {
-    let code = it.next().unwrap_or("0").parse::<i32>()?;
-    std::process::exit(code);
+fn handle_exit(mut tokens: SplitWhitespace) -> Result<()> {
+    let code = tokens.next().unwrap_or("0").parse::<i32>()?;
+    std::process::exit(code)
 }
 
-fn handle_type(mut it: std::str::SplitWhitespace) -> Result<()> {
-    let arg = it
+fn handle_type(mut tokens: SplitWhitespace, paths: &HashSet<&str>) -> Result<()> {
+    let arg = tokens
         .next()
         .ok_or_else(|| Error::msg("type: missing argument"))?;
+
+    let builtins = HashSet::from(["echo", "exit", "type"]);
+
     match arg {
-        arg if HashSet::from(["echo", "exit", "type"]).contains(arg) => {
-            println!("{} is a shell builtin", arg);
-            Ok(())
+        _ if builtins.contains(arg) => Ok(println!("{} is a shell builtin", arg)),
+        _ => {
+            let path = paths.iter().find_map(|path| {
+                let cmd = format!("{}/{}", path, arg);
+                std::fs::metadata(&cmd).ok().map(|_| cmd)
+            });
+            path.map_or_else(
+                || Err(Error::msg(format!("{}: not found", arg))),
+                |path| Ok(println!("{} is {}", arg, path)),
+            )
         }
-        _ => Err(Error::msg(format!("{}: not found", arg))),
     }
 }
